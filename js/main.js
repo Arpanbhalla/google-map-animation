8eee18eafd7c79ade96bc6189458b2e18a342f31
Arpan Bhalla <arpanbhalla@gmail.com>
2017-05-09 21:35:54 +1000

    Added Bounse Animation

--- js/main.js
+++ js/main.js
@@ -2,37 +2,34 @@ var geocoder;
 var mapCentre;
 var map;
 var coordinates = [];
-// var marker_icon = 'http://maps.google.com/mapfiles/kml/shapes/gas_stations.png';
 
 function initialize() {
 
   // Centre the map to Australia
-        var country = "Australia";
-        var myOptions = {
-          zoom: 4,
-          mapTypeId: google.maps.MapTypeId.HYBRID
-        };
-        var map = new google.maps.Map(document.getElementById("map"), myOptions);
-        // new google.maps.Geocoder(); => Converts location to lat /long
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode({ 'address': country }, function (results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-          } else {
-            alert("Could not find location: " + location);
-          }
-        });
+  var country = "Australia";
+  var myOptions = {
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+  // new google.maps.Geocoder(); => Converts location to lat /long
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({ 'address': country }, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+    } else {
+      alert("Could not find location: " + location);
+    }
+  });
 
   // Read from data input type json
-  $.getJSON("http://localhost:3000/data/stores.json", function (stores) {
-
+  $.getJSON("http://localhost:3000/data/cities.json", function (cities) {
     // loop all the markers
-    $.each(stores, function (i, store_detail) {
-
+    $.each(cities, function (i, city_detail) {
       // add marker to map
-      console.log(store_detail);
-      lat = store_detail.Latitude;
-      lng = store_detail.Longitude;
+      console.log(city_detail);
+      lat = city_detail.Latitude;
+      lng = city_detail.Longitude;
       var latLng = new google.maps.LatLng(lat, lng);
       coordinates.push(latLng);
     });
@@ -40,13 +37,24 @@ function initialize() {
   var i = 0;
   var interval = setInterval(function () {
     var marker = new google.maps.Marker({
-        map: map,
-        draggable: false,
-        // icon:  marker_icon,
-        animation: google.maps.Animation.DROP,
-        position: coordinates[i]
+      map: map,
+      draggable: false,
+      animation: google.maps.Animation.DROP,
+      position: coordinates[i]
     });
+    new google.maps.event.addListener(marker, 'click', function (e) { toggleBounce(marker) });
     i++;
     if (i >= coordinates.length) clearInterval(interval);
-  }, 10);
+  }, 400);
+}
+
+function toggleBounce(marker) {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function () {
+      marker.setAnimation(null);
+    }, 1400);
+  }
 }
